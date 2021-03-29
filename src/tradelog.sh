6d9e55dd33562292609d225ddb30a35603622dbb
ceskyDJ <48548230+ceskyDJ@users.noreply.github.com>
2021-03-29 18:42:12 +0200

    Updated source file structure

--- src/tradelog.sh
+++ src/tradelog.sh
@@ -13,64 +13,6 @@ if [ "$SHELL" != "/bin/bash" ]; then
   exit $SHELL_ERROR
 fi
 
-########################################################################################################################
-# Functions
-# Writes script usage
-function write_help() {
-  cat << EOF
-Usage: $SCRIPT [-h|--help] [FILTER]... [COMMAND] [LOG_FILE]...
-Stock exchange trading logs analyzer
-
-You can use one of these commands:
-  list-tick      shows the list of contained tickers
-  profit         counts total profit from closed positions
-  pos            shows values of currently owned positions ordered by value
-                   descending (the biggest first)
-  last-price     shows the last-know price for each ticker
-  hist-ord       shows a histogram with number of transaction for each ticker
-  graph-pos      shows a graph of the owned tickers' values
-
-There are these filters available to use:
-  -a DATETIME    uses records after DATETIME only (excludes DATETIME)
-                   more occurrences mean their intersection
-  -b DATETIME    uses records before DATETIME only (excludes DATETIME)
-                   more occurrences mean their intersection
-  -t TICKER      uses only records with selected ticker(s) - could be used
-                   multiple times
-
-Other options are:
-  -w WIDTH       sets size of the graphs' width
-  -h, --help     show this usage information
-
-DATETIME is in the format YYYY-mm-dd HH:MM:SS, where YYYY is the full year
-(e.g., 2021), mm is the two digits month (e.g., 03 for March), dd is
-the two digits day of month (e.g., 01), HH is the hour in 24-hour format
-(00..23), MM are minutes with leading zero (e.g., 03) and SS are seconds
-with leading zero (e.g., 09).
-
-TICKER is the name of stock exchange ticker (symbol) - unique
-identification of tradable item (e.g., APPL for Apple, a.s.).
-
-WIDTH is the maximum number of characters to be written in graph.
-The biggest number will have WIDTH characters (# or !) and smaller
-numbers will have proportional number of character according its
-value compared to the biggest one.
-EOF
-}
-
-# Counts intersection of two datetime values
-function datetime_intersect() {
-  local first second intersect
-
-  first=$(date -d "$1" "+%s")
-  second=$(date -d "$2" "+%s")
-
-  intersect=$(( (first + second) / 2 ))
-  date -d "@$intersect" "+%Y-%m-%d %H:%M:%S"
-}
-########################################################################################################################
-
-
 # Reformat arguments to stable format
 # TODO: remove -v
 opts=$(getopt -o ha:b:t:w:v -l help: -n "$SCRIPT" -s bash -- "$@") || exit $ARG_ERROR
@@ -191,3 +133,68 @@ fi
 
 # TODO: for future data processing
 eval "$input | cat"
+
+
+########################################################################################################################
+
+# Writes script usage
+# Stdout: script usage to the stdout
+function write_help() {
+  cat << EOF
+Usage: $SCRIPT [-h|--help] [FILTER]... [COMMAND] [LOG_FILE]...
+Stock exchange trading logs analyzer
+
+You can use one of these commands:
+  list-tick      shows the list of contained tickers
+  profit         counts total profit from closed positions
+  pos            shows values of currently owned positions ordered by value
+                   descending (the biggest first)
+  last-price     shows the last-know price for each ticker
+  hist-ord       shows a histogram with number of transaction for each ticker
+  graph-pos      shows a graph of the owned tickers' values
+
+There are these filters available to use:
+  -a DATETIME    uses records after DATETIME only (excludes DATETIME)
+                   more occurrences mean their intersection
+  -b DATETIME    uses records before DATETIME only (excludes DATETIME)
+                   more occurrences mean their intersection
+  -t TICKER      uses only records with selected ticker(s) - could be used
+                   multiple times
+
+Other options are:
+  -w WIDTH       sets size of the graphs' width
+  -h, --help     show this usage information
+
+DATETIME is in the format YYYY-mm-dd HH:MM:SS, where YYYY is the full year
+(e.g., 2021), mm is the two digits month (e.g., 03 for March), dd is
+the two digits day of month (e.g., 01), HH is the hour in 24-hour format
+(00..23), MM are minutes with leading zero (e.g., 03) and SS are seconds
+with leading zero (e.g., 09).
+
+TICKER is the name of stock exchange ticker (symbol) - unique
+identification of tradable item (e.g., APPL for Apple, a.s.).
+
+WIDTH is the maximum number of characters to be written in graph.
+The biggest number will have WIDTH characters (# or !) and smaller
+numbers will have proportional number of character according its
+value compared to the biggest one.
+EOF
+}
+
+# Counts intersection of two datetime values
+# Args:
+#  - first datetime
+#  - second datetime
+# Stdout: intersection of provided datetime values in format YYYY-mm-dd HH:MM:SS (see usage)
+# Stderr: invalid date (from date command)
+function datetime_intersect() {
+  local first second intersect
+
+  first=$(date -d "$1" "+%s")
+  second=$(date -d "$2" "+%s")
+
+  intersect=$(( (first + second) / 2 ))
+  date -d "@$intersect" "+%Y-%m-%d %H:%M:%S"
+}
+
+########################################################################################################################
